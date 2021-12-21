ab80fed50068db5b0a49d0f446efb150ea17862a
Speeder07 <speeder.x.one@gmail.com>
2021-12-21 10:06:36 +0100

    Pierwsza funkcjonująca siatka

--- projekt_reactgame/src/index.js
+++ projekt_reactgame/src/index.js
@@ -4,10 +4,51 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function GenerateArray(x, y) {
+  return Array(x).fill(0).map(row => new Array(y).fill(0))
+}
+
+function DisplayGrid(arr) {
+  for (let i of arr) {
+    for (let j of i) {
+      <div>{j}</div>
+    }
+    <br/>
+  }
+  return arr;
+}
+
+class Board extends React.Component{
+
+
+
+  constructor(props) {
+    super(props);
+    
+  }
+
+  render() {
+    const arr = GenerateArray(15,7);
+    return (
+      <div className="board">
+        {arr.map((items, index)=>{
+          return(
+            <div>
+              {items.map((subItem, sIndex)=>{
+                return <div>{subItem}</div>;
+              })}
+              <br/>
+            </div>
+            
+          );
+        })}
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Board/>,
   document.getElementById('root')
 );
 
