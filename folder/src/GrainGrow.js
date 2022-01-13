16caef0704672e90ad632818b7436916195a9608
dawid-szymanek <96084799+dawid-szymanek@users.noreply.github.com>
2022-01-13 20:15:04 +0100

    Add files via upload

---
+++ folder/src/GrainGrow.js
@@ -0,0 +1,195 @@
+import * as Fr from './Fractions';
+import * as Ix from './Pallete';
+
+class Counter
+{
+  constructor(fraction, count)
+  {
+    this.fraction = fraction;
+    this.count = count;
+  }
+}
+
+
+export function Rule01(array, temp_array, sx, sy) {
+    
+    let tab = new Array(0);
+
+    for (let x = sx-1; x <= sx+1; x++) {
+      for (let y = sy-1; y <= sy+1; y++) {
+
+        if (x == sx && y == sy) 
+          continue;
+        
+        if (array[x]===undefined) 
+          continue;
+        
+        if (array[x][y]===undefined) 
+          continue;
+  
+        if (array[x][y].id > 0) 
+        {
+            let asigned = false;
+            tab.forEach(element => {
+              if (element.fraction.id === array[x][y].id) {
+                element.count += 1;
+                asigned = true;
+              }
+            });
+            if (!asigned) {
+              tab.push(new Counter(array[x][y], 1));
+              
+            }
+        }
+      }
+      
+    }
+    if (tab.length != 0) {
+        let max = tab[0];
+        for (const element of tab) {
+          if (element.count > max.count) {
+            max = element;
+          }
+        }
+        if (max.count >= 5) {
+            temp_array[sx][sy] = max.fraction;
+            return true;
+        }
+    }
+    return false;
+}
+
+export function Rule02(array, temp_array, sx, sy) {
+    
+    let tab = new Array(0);
+
+    for (let x = sx-1; x <= sx+1; x++) {
+      for (let y = sy-1; y <= sy+1; y++) {
+        
+        if (x != sx && y != sy) 
+            continue;
+
+        if (x == sx && y == sy) 
+            continue;
+        
+        if (array[x]===undefined) 
+            continue;
+        
+        if (array[x][y]===undefined) 
+            continue;
+  
+        if (array[x][y].id > 0) 
+        {
+            let asigned = false;
+            tab.forEach(element => {
+              if (element.fraction.id === array[x][y].id) {
+                element.count += 1;
+                asigned = true;
+              }
+            });
+            if (!asigned) {
+              tab.push(new Counter(array[x][y], 1));
+            }
+        }
+      }
+    }
+    if (tab.length != 0) {
+        let max = tab[0];
+        for (const element of tab) {
+          if (element.count > max.count) {
+            max = element;
+          }
+        }
+        if (max.count >= 3) {
+            temp_array[sx][sy] = max.fraction;
+        }
+    }
+}
+
+export function Rule03(array, temp_array, sx, sy) {
+    
+    let tab = new Array(0);
+
+    for (let x = sx-1; x <= sx+1; x++) {
+      for (let y = sy-1; y <= sy+1; y++) {
+
+        if (x == sx || y == sy) 
+        continue;
+
+        if (array[x]===undefined) 
+          continue;
+        
+        if (array[x][y]===undefined) 
+          continue;
+  
+        if (array[x][y].id > 0) 
+        {
+            let asigned = false;
+            tab.forEach(element => {
+              if (element.fraction.id === array[x][y].id) {
+                element.count += 1;
+                asigned = true;
+              }
+            });
+            if (!asigned) {
+              tab.push(new Counter(array[x][y], 1));
+            }
+        }
+      }
+    }
+    if (tab.length != 0) {
+        let max = tab[0];
+        for (const element of tab) {
+          if (element.count > max.count) {
+            max = element;
+          }
+        }
+        if (max.count >= 3) {
+            temp_array[sx][sy] = max.fraction;
+        }
+    }
+}
+
+export function Rule04(array, temp_array, sx, sy) {
+    
+    let tab = new Array(0);
+
+    for (let x = sx-1; x <= sx+1; x++) {
+      for (let y = sy-1; y <= sy+1; y++) {
+
+        if (x == sx && y == sy) 
+          continue;
+        
+        if (array[x]===undefined) 
+          continue;
+        
+        if (array[x][y]===undefined) 
+          continue;
+  
+        if (array[x][y].id > 0) 
+        {
+            let asigned = false;
+            tab.forEach(element => {
+              if (element.fraction.id === array[x][y].id) {
+                element.count += 1;
+                asigned = true;
+              }
+            });
+            if (!asigned) {
+              tab.push(new Counter(array[x][y], 1));
+            }
+        }
+      }
+    }
+    if (tab.length != 0) {
+        let max = tab[0];
+        for (const element of tab) {
+          if (element.count > max.count) {
+            max = element;
+          }
+        }
+        if (Math.floor(Math.random() * 101) <= Ix.GlobalRandom) {
+            temp_array[sx][sy] = max.fraction;
+        }
+    }
+}
